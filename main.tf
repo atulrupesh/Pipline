4eb190001556e4e9a41e47b38e9154a460df4bc1
atulrupesh <rupesh.kumar8612@gmail.com>
2025-08-09 18:36:37 +0530

    done

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_resource_group" "rg1rk" {
+  name     = "rg-0506"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+resource "azurerm_storage_account" "kumaratula" {
+  name                     = "kumaratula0506"
+  resource_group_name      = azurerm_resource_group.rg1rk.name
+  location                 = azurerm_resource_group.rg1rk.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  
+}
