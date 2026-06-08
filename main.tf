b677bb391aa9ea9f97a4e7bd846ac64855e174aa
atulrupesh <rupeshwork555@gmail.com>
2026-06-08 15:10:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,35 +2,3 @@ resource "azurerm_resource_group" "rg" {
   name     = "rup-rg"
   location = "West Europe"
 }
-
-resource "azurerm_kubernetes_cluster" "k8n" {
-  name                = "aks-cluster"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "exampleaks1"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2s_v3"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    Environment = "Production"
-  }
-}
-
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.k8n.kube_config[0].client_certificate
-  sensitive = true
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.k8n.kube_config_raw
-
-  sensitive = true
-}
